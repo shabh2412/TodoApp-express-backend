15d0f1e1e55332aea9b020ca3f6cee5665914b0a
Rishabh Panesar <rishabhpanesar@gmail.com>
2022-08-03 13:34:54 +0530

    updated port

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.use(
 );
 app.use("/todos", todosRouter);
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
 	console.log(`server running in port: ${PORT}`);
 });
