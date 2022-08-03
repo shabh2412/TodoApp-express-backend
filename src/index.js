ea80874c1d4dae7a64fad9bff7467555ee70ad4b
Rishabh Panesar <rishabhpanesar@gmail.com>
2022-08-03 13:58:41 +0530

    added cors back

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ const todosRouter = require("./controller/todos.controller");
 const cors = require("cors");
 
 const app = express();
-// app.use(
-// 	cors({
-// 		origin: [`http://localhost:3000`, "http://192.168.0.103:3000"],
-// 	})
-// );
+app.use(
+	cors({
+		origin: [`http://localhost:3000`, "http://192.168.0.103:3000"],
+	})
+);
 app.use("/todos", todosRouter);
 app.get("/", (req, res) => {
 	res.send("Hello from express app");
