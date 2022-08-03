9d0b8da222d3eac2e2881a77098f24388e575433
Rishabh Panesar <rishabhpanesar@gmail.com>
2022-08-03 13:40:52 +0530

    added proper path for start script, and handling get request on / url

--- src/index.js
+++ src/index.js
@@ -10,8 +10,11 @@ app.use(
 	})
 );
 app.use("/todos", todosRouter);
+app.get("/", (req, res) => {
+	res.send("Hello from express app");
+});
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
 	console.log(`server running in port: ${PORT}`);
 });
