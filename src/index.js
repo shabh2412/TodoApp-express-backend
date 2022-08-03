80456dad3278d2fcd3eb93ff321cdfb9b423614a
Rishabh Panesar <rishabhpanesar@gmail.com>
2022-08-03 22:10:46 +0530

    fixed cors link

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ app.use(
 			`http://localhost:3000`,
 			"http://192.168.0.103:3000",
 			"https://todo-react-express-shabh2412.vercel.app",
-			"https://todo-react-express.vercel.app"
 		],
 	})
 );
